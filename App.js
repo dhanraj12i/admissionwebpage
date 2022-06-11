5bc3e9cc7a57bb248e29534bb9d6c909b491053d
52114_DhanrajP <dhanrajpatil967@gmail.com>
2022-06-11 16:22:27 +0530

    login section page created

---
+++ App.js
@@ -0,0 +1,30 @@
+<div class="Container">
+<div class="e">
+    <!-- <div class="col-lg-5">
+    <img src="./images/loginPage.jpg" class="img-fluid" alt="">
+</div> -->
+    <div class="col-lg-7">
+        <form>
+            <div class="form-row">
+                <div class="col-lg-7">
+                    <input type="email" placeholder="Email Address" class="form-control">
+                </div>
+            </div>
+            <div class="form-row">
+                <div class="col-lg-7">
+                    <input type="password" placeholder="*********" class="form-control">
+                </div>
+            </div>
+            <div class="form-row">
+                <div class="col-lg-7">
+                    <button type="button" class="btnl">Login</button>
+                </div>
+            </div>
+            <a href="#">Forget password</a>
+            <section>
+                <p>New user?<a href="#"> Register here </a></p>
+            </section>
+        </form>
+    </div>
+</div>
+</div>
